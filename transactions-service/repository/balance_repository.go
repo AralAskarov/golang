07f1ce26c42e5ba904d0d418be1f7540ce8cec2b
aras0125 <aral.askarov@netcracker.com>
2025-04-30 15:39:39 +0500

    123

--- transactions-service/repository/balance_repository.go
+++ transactions-service/repository/balance_repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"encoding/hex"
+	"strings"
 )
 
 type PostgresBalanceRepository struct {
@@ -13,34 +15,55 @@ func NewPostgresBalanceRepository(db *sql.DB) BalanceRepository {
 	return &PostgresBalanceRepository{db: db}
 }
 
+func UUIDToHexBin(uuid string) (string, error) {
+	uuid = strings.ReplaceAll(uuid, "-", "")
+	bytes, err := hex.DecodeString(uuid)
+	if err != nil {
+		return "", err
+	}
+	return "0x" + strings.ToUpper(hex.EncodeToString(bytes)), nil
+}
+
 func (r *PostgresBalanceRepository) UpdateBalanceByUUID(ctx context.Context, uuid string, amount int) error {
+	hexUUID, err := UUIDToHexBin(uuid)
+	if err != nil {
+		return err
+	}
 	query := `
 		UPDATE users
 		SET balance = balance + ?
-		WHERE uuid = UUID_TO_BIN(REPLACE(?, '-', ''), 1)
+		WHERE uuid = ?
 	`
-	_, err := r.db.ExecContext(ctx, query, amount, uuid)
+	_, err = r.db.ExecContext(ctx, query, amount, hexUUID)
 	return err
 }
 
 func (r *PostgresBalanceRepository) UpdateBalanceByUUIDWithDrawal(ctx context.Context, uuid string, amount int) error {
+	hexUUID, err := UUIDToHexBin(uuid)
+	if err != nil {
+		return err
+	}
 	query := `
 		UPDATE users
 		SET balance = balance - ?
-		WHERE uuid = UUID_TO_BIN(REPLACE(?, '-', ''), 1);
+		WHERE uuid = ?
 	`
-	_, err := r.db.ExecContext(ctx, query, amount, uuid)
+	_, err = r.db.ExecContext(ctx, query, amount, hexUUID)
 	return err
 }
 
 func (r *PostgresBalanceRepository) IsThereEnoughMoneyByUUID(ctx context.Context, uuid string, amount int) (bool, error) {
+	hexUUID, err := UUIDToHexBin(uuid)
+	if err != nil {
+		return false, err
+	}
 	query := `
 		SELECT balance
 		FROM users
-		WHERE uuid = UUID_TO_BIN(REPLACE(?, '-', ''), 1)
+		WHERE uuid = ?
 	`
 	var balance int
-	err := r.db.QueryRowContext(ctx, query, uuid).Scan(&balance)
+	err = r.db.QueryRowContext(ctx, query, hexUUID).Scan(&balance)
 	if err != nil {
 		return false, err
 	}
